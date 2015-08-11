19f986b4f886e70451dcdc462725ea3cc136c6c3
mhussainm-mac <gitmanashia@gmail.com>
2015-08-11 09:30:47 +0530

    Mobile icons and splash screen

--- mobile-config.js
+++ mobile-config.js
@@ -1,23 +1,26 @@
 
 App.accessRule('*');
 
-App.icons({
-  'android_ldpi': 'resources/icons/icon-36x36.png',
-  'android_mdpi': 'resources/icons/icon-48x48.png',
-  'android_tvdpi': 'resources/icons/icon-64x64.png',  
-  'android_hdpi': 'resources/icons/icon-72x72.png',
-  'android_xhdpi': 'resources/icons/icon-96x96.png',
-  'android_xxhdpi': 'resources/icons/icon-144x144.png',
-  'android_xxxhdpi': 'resources/icons/icon-192x192.png'  
+App.info({
+  name: 'Healthcare Connect',
+  description: 'Healthcare on Social steroids',
+  author: 'Bank Tank TCS Insurance 2',
+  email: 'mhmanashia@gmail.com',
+  website: 'http://mhussainm.com'
+});
+
+App.icons({  
+	'android_ldpi': 'resources/icons/icon-36x36.png',
+	'android_mdpi': 'resources/icons/icon-48x48.png',
+	'android_hdpi': 'resources/icons/icon-72x72.png',
+	'android_xhdpi': 'resources/icons/icon-96x96.png'
 });
 
 App.launchScreens({
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-640x960.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-960x1600.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-1280x1960.png'    
+	'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+	'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+	'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+	'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png'
 });
 
 App.setPreference('orientation','portrait');
